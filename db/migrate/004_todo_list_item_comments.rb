229f4931ab7c8cc2989202d7185a1eb7896f7047
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2023-07-25 17:08:29 +0200

    Update 004_todo_list_item_comments.rb
    Optimize db script for migration.

--- db/migrate/004_todo_list_item_comments.rb
+++ db/migrate/004_todo_list_item_comments.rb
@@ -1,5 +1,15 @@
 class TodoListItemComments < ActiveRecord::Migration[4.2]
-  def change
-    add_column :issue_todo_list_items, :comment, :text
+  def up
+    # Add the new column if it doesn't exist
+    unless column_exists?(:issue_todo_list_items, :comment)
+      add_column :issue_todo_list_items, :comment, :text
+    end
+  end
+
+  def down
+    # Remove the column if it exists
+    if column_exists?(:issue_todo_list_items, :comment)
+      remove_column :issue_todo_list_items, :comment
+    end
   end
 end
