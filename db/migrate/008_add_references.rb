972dc66bcb3eef757dd4cf24239924bddc37ef89
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2023-07-25 17:18:03 +0200

    Update 008_add_references.rb
    Optimize db script for migration.

--- db/migrate/008_add_references.rb
+++ db/migrate/008_add_references.rb
@@ -1,50 +1,62 @@
 class AddReferences < ActiveRecord::Migration[4.2]
   def up
     # Modifying issue_todo_lists
-    change_table :issue_todo_lists do |t|
-      t.rename :project_identifier, :project_id
-      t.rename :created_by, :created_by_id
-      t.rename :last_updated_by, :last_updated_by_id
+    if column_exists?(:issue_todo_lists, :project_identifier)
+      rename_column :issue_todo_lists, :project_identifier, :project_id
+      IssueTodoList.reset_column_information
+      IssueTodoList.all.each do |list|
+        list.update!(project_id: Project.where(identifier: list.project_id).first&.id)
+      end
     end
 
-    add_foreign_key :issue_todo_lists, :projects
-    add_foreign_key :issue_todo_lists, :users, column: :created_by_id
-    add_foreign_key :issue_todo_lists, :users, column: :last_updated_by_id
+    unless column_exists?(:issue_todo_lists, :created_by_id)
+      rename_column :issue_todo_lists, :created_by, :created_by_id
+    end
 
-    # Data validation and correction
-    IssueTodoList.find_each do |list|
-      unless Project.exists?(id: list.project_id)
-        list.update!(project_id: Project.first.id) # Or any default project you want
-      end
-
-      unless User.exists?(id: list.created_by_id)
-        list.update!(created_by_id: User.first.id) # Or any default user you want
-      end
-
-      unless User.exists?(id: list.last_updated_by_id)
+    unless column_exists?(:issue_todo_lists, :last_updated_by_id)
+      rename_column :issue_todo_lists, :last_updated_by, :last_updated_by_id
+      IssueTodoList.reset_column_information
+      IssueTodoList.where(last_updated_by_id: nil).each do |list|
         list.update!(last_updated_by_id: User.first.id) # Or any default user you want
       end
     end
 
     # Modifying issue_todo_list_items
-    add_reference :issue_todo_list_items, :issue_todo_list, foreign_key: true, index: true
-    add_reference :issue_todo_list_items, :issue, foreign_key: true, index: true
+    unless column_exists?(:issue_todo_list_items, :issue_todo_list_id)
+      add_reference :issue_todo_list_items, :issue_todo_list, foreign_key: true, index: true
+    end
+
+    unless column_exists?(:issue_todo_list_items, :issue_id)
+      add_reference :issue_todo_list_items, :issue, foreign_key: true, index: true
+    end
   end
 
   def down
     # Modifying issue_todo_lists
-    remove_foreign_key :issue_todo_lists, :projects
-    remove_foreign_key :issue_todo_lists, :users, column: :created_by_id
-    remove_foreign_key :issue_todo_lists, :users, column: :last_updated_by_id
+    if column_exists?(:issue_todo_lists, :project_id)
+      remove_foreign_key :issue_todo_lists, :projects
+      rename_column :issue_todo_lists, :project_id, :project_identifier
+    end
 
-    change_table :issue_todo_lists do |t|
-      t.rename :project_id, :project_identifier
-      t.rename :created_by_id, :created_by
-      t.rename :last_updated_by_id, :last_updated_by
+    if column_exists?(:issue_todo_lists, :created_by_id)
+      remove_foreign_key :issue_todo_lists, :users, column: :created_by_id
+      rename_column :issue_todo_lists, :created_by_id, :created_by
+    end
+
+    if column_exists?(:issue_todo_lists, :last_updated_by_id)
+      remove_foreign_key :issue_todo_lists, :users, column: :last_updated_by_id
+      rename_column :issue_todo_lists, :last_updated_by_id, :last_updated_by
     end
 
     # Modifying issue_todo_list_items
-    remove_reference :issue_todo_list_items, :issue_todo_list, index: true
-    remove_reference :issue_todo_list_items, :issue, index: true
+    if column_exists?(:issue_todo_list_items, :issue_todo_list_id)
+      remove_foreign_key :issue_todo_list_items, :issue_todo_lists
+      remove_reference :issue_todo_list_items, :issue_todo_list, index: true
+    end
+
+    if column_exists?(:issue_todo_list_items, :issue_id)
+      remove_foreign_key :issue_todo_list_items, :issues
+      remove_reference :issue_todo_list_items, :issue, index: true
+    end
   end
 end
