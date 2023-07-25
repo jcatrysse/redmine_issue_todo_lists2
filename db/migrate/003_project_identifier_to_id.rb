a535a190bcc5febea09364ed14206ea187fb7261
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2023-07-26 00:11:59 +0200

    Update 003_project_identifier_to_id.rb
    Optimize speed and mysql specific code

--- db/migrate/003_project_identifier_to_id.rb
+++ db/migrate/003_project_identifier_to_id.rb
@@ -2,11 +2,11 @@ class ProjectIdentifierToId < ActiveRecord::Migration[4.2]
   def up
     # Add new column if it doesn't exist
     unless column_exists?(:issue_todo_lists, :project_id)
-      add_column :issue_todo_lists, :project_id, :integer, null: true, after: :id
+      add_column :issue_todo_lists, :project_id, :integer, null: true
 
       # Convert project_identifier to project_id
-      IssueTodoList.all.each do |list|
-        list.update(project_id: Project.where(identifier: list.project_identifier).first&.id)
+      IssueTodoList.find_each do |list|
+        list.update(project_id: Project.find_by(identifier: list.project_identifier)&.id)
       end
     end
 
@@ -19,11 +19,11 @@ class ProjectIdentifierToId < ActiveRecord::Migration[4.2]
   def down
     # Revert back - Add old column if it doesn't exist
     unless column_exists?(:issue_todo_lists, :project_identifier)
-      add_column :issue_todo_lists, :project_identifier, :string, null: true, after: :id
+      add_column :issue_todo_lists, :project_identifier, :string, null: true
 
       # Convert project_id back to project_identifier
-      IssueTodoList.all.each do |list|
-        list.update(project_identifier: Project.where(id: list.project_id).first&.identifier)
+      IssueTodoList.find_each do |list|
+        list.update(project_identifier: Project.find_by(id: list.project_id)&.identifier)
       end
     end
 
