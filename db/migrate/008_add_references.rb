b30d53b96dc81441695ac56d3631becaa13d73e9
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2023-07-25 18:46:41 +0200

    Update 008_add_references.rb
    Make some corrections on things already done in the past, that were double and useless

--- db/migrate/008_add_references.rb
+++ db/migrate/008_add_references.rb
@@ -1,14 +1,6 @@
 class AddReferences < ActiveRecord::Migration[4.2]
   def up
     # Modifying issue_todo_lists
-    if column_exists?(:issue_todo_lists, :project_identifier)
-      rename_column :issue_todo_lists, :project_identifier, :project_id
-      IssueTodoList.reset_column_information
-      IssueTodoList.all.each do |list|
-        list.update!(project_id: Project.where(identifier: list.project_id).first&.id)
-      end
-    end
-
     unless column_exists?(:issue_todo_lists, :created_by_id)
       rename_column :issue_todo_lists, :created_by, :created_by_id
     end
@@ -22,40 +14,55 @@ class AddReferences < ActiveRecord::Migration[4.2]
     end
 
     # Modifying issue_todo_list_items
-    unless column_exists?(:issue_todo_list_items, :issue_todo_list_id)
+    begin
       add_reference :issue_todo_list_items, :issue_todo_list, foreign_key: true, index: true
+    rescue StandardError => e
+      warn "Could not add foreign key constraint to issue_todo_list_items for issue_todo_list: #{e.message}"
     end
 
-    unless column_exists?(:issue_todo_list_items, :issue_id)
+    begin
       add_reference :issue_todo_list_items, :issue, foreign_key: true, index: true
+    rescue StandardError => e
+      warn "Could not add foreign key constraint to issue_todo_list_items for issue: #{e.message}"
     end
   end
 
   def down
     # Modifying issue_todo_lists
-    if column_exists?(:issue_todo_lists, :project_id)
-      remove_foreign_key :issue_todo_lists, :projects
-      rename_column :issue_todo_lists, :project_id, :project_identifier
-    end
-
     if column_exists?(:issue_todo_lists, :created_by_id)
-      remove_foreign_key :issue_todo_lists, :users, column: :created_by_id
+      begin
+        remove_foreign_key :issue_todo_lists, :users, column: :created_by_id
+      rescue StandardError => e
+        warn "Could not remove foreign key constraint from issue_todo_lists for created_by_id: #{e.message}"
+      end
       rename_column :issue_todo_lists, :created_by_id, :created_by
     end
 
     if column_exists?(:issue_todo_lists, :last_updated_by_id)
-      remove_foreign_key :issue_todo_lists, :users, column: :last_updated_by_id
+      begin
+        remove_foreign_key :issue_todo_lists, :users, column: :last_updated_by_id
+      rescue StandardError => e
+        warn "Could not remove foreign key constraint from issue_todo_lists for last_updated_by_id: #{e.message}"
+      end
       rename_column :issue_todo_lists, :last_updated_by_id, :last_updated_by
     end
 
     # Modifying issue_todo_list_items
     if column_exists?(:issue_todo_list_items, :issue_todo_list_id)
-      remove_foreign_key :issue_todo_list_items, :issue_todo_lists
+      begin
+        remove_foreign_key :issue_todo_list_items, :issue_todo_lists
+      rescue StandardError => e
+        warn "Could not remove foreign key constraint from issue_todo_list_items for issue_todo_list_id: #{e.message}"
+      end
       remove_reference :issue_todo_list_items, :issue_todo_list, index: true
     end
 
     if column_exists?(:issue_todo_list_items, :issue_id)
-      remove_foreign_key :issue_todo_list_items, :issues
+      begin
+        remove_foreign_key :issue_todo_list_items, :issues
+      rescue StandardError => e
+        warn "Could not remove foreign key constraint from issue_todo_list_items for issue_id: #{e.message}"
+      end
       remove_reference :issue_todo_list_items, :issue, index: true
     end
   end
