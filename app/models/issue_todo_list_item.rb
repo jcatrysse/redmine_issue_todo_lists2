dde59d9b86d7f30e9a6f1402fc696c88f1577d56
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2023-07-25 16:22:36 +0200

    Update issue_todo_list_item.rb
    Correct visible? method

--- app/models/issue_todo_list_item.rb
+++ app/models/issue_todo_list_item.rb
@@ -15,7 +15,9 @@ class IssueTodoListItem < ActiveRecord::Base
     todo_list.force_updated
     todo_list.save
   end
+
   def visible?(user = User.current)
-    user.allowed_to?(:view_issue_todo_lists, @project, global: true)
+    project = issue_todo_list&.project
+    project && user.allowed_to?(:view_issue_todo_lists, project, global: true)
   end
 end
